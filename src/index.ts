769ee552f82e7df4edec02c58ffc971872b6de8e
Deepak <ideepakrabindra@gmail.com>
2024-04-27 21:59:17 +0530

    - **fix**: Update file extensions in nodemon configuration - **fix**: Add type annotations for express in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 
-const app = express();
+const app: Express = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Okk');
 });
 
