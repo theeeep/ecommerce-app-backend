f0212e1e6a39beb13581302858dfb1032270e11f
Deepak <ideepakrabindra@gmail.com>
2024-04-29 19:40:58 +0530

    style[controllers]: add TODO comment for creating validator in createProduct function

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,8 +1,10 @@
 import prisma from 'config/db.config';
 import { Request, Response } from 'express';
 
+//? ---> Create Product
 export const createProduct = async (req: Request, res: Response) => {
-  // create validator for this request
+  // TODO create validator for this request
+
   const product = await prisma.product.create({
     data: {
       ...req.body,
